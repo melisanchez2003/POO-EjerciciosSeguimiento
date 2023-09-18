3c46210f5e84d12ce73a6482097a04b7150417d4
melisanchez2003 <108958674+melisanchez2003@users.noreply.github.com>
2023-09-18 18:44:14 -0500

    Tercer commit

---
+++ POOER7IG/src/main/java/com/mycompany/pooer7ig/POOER7IG.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooer7ig;
+
+
+public class POOER7IG {
+
+    public static void main(String[] args) {
+       Formulario form= new Formulario();
+       form.setVisible(true);
+        
+    }
+}
