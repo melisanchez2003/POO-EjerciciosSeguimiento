4c878e2cf7dfcd9410bbe71570bc7c6bd7fe2c86
melisanchez2003 <108958674+melisanchez2003@users.noreply.github.com>
2023-11-12 15:48:46 -0500

    Quinto commit

---
+++ POOE82/src/main/java/com/mycompany/pooe82/Notas.java
@@ -0,0 +1,52 @@
+
+package com.mycompany.pooe82;
+
+
+public class Notas {
+    double[] listaNotas;
+    
+    public Notas() {
+    listaNotas = new double[5]; 
+    }
+    
+    double calcularPromedio() {
+        double suma = 0;
+        for(int i=1; i < listaNotas.length; i++) {
+            suma = suma + listaNotas[i]; 
+        }
+        return (suma / listaNotas.length);
+    }
+    
+    double calcularDesviacion() {
+        
+    
+        double prom = calcularPromedio(); 
+
+        double suma1 = 0;
+        
+        for(int i=0; i < listaNotas.length; i++) {
+            suma1 += Math.pow(listaNotas[i] - prom, 2 );
+        }
+        return Math.sqrt (suma1/listaNotas.length );
+    
+    }
+    double calcularMenor() {
+        double menor = listaNotas[0]; 
+        for(int i=0; i < listaNotas.length; i++) {
+            if (listaNotas[i] < menor) {
+                menor = listaNotas[i];
+            }
+        }
+    return menor;
+    }
+    
+    double calcularMayor() {
+        double mayor = listaNotas[0]; 
+        for(int i=0; i < listaNotas.length; i++) { 
+            if (listaNotas[i] > mayor) {
+                mayor = listaNotas[i];
+                    }
+        }
+        return mayor;
+    }
+}
---
+++ POOE82/src/main/java/com/mycompany/pooe82/POOE82.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.pooe82;
+
+public class POOE82 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true);
+     }
+}
