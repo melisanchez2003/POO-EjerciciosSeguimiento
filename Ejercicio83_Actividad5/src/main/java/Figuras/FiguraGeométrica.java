2ae43357ee74d64256e8f27ae7e44351160f5537
ipalomino1 <143232229+ipalomino1@users.noreply.github.com>
2023-11-13 23:03:04 -0500

    Add files via upload

---
+++ Ejercicio83_Actividad5/src/main/java/Figuras/FiguraGeométrica.java
@@ -0,0 +1,23 @@
+
+package Figuras;
+
+public class FiguraGeométrica {
+    
+    private double volumen;
+    
+    private double superficie;
+    
+    public void setVolumen(double volumen){
+        this.volumen = volumen;
+    }
+    
+    public void setSuperficie(double superficie){
+        this.superficie = superficie;
+    }
+    public double getVolumen(){
+        return this.volumen;
+    }
+    public double getSuperficie(){
+        return this.superficie;
+    }
+}
---
+++ Ejercicio83_Actividad5/src/main/java/Figuras/Principal.java
@@ -0,0 +1,16 @@
+
+package Figuras;
+
+
+public class Principal {
+    
+   public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+        miVentanaPrincipal.setResizable(false);
+
+   }
+    
+    
+}
