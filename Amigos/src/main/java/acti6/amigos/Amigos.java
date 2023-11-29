adc78dd25fb4e0a3f418ca0b93f6c5d8635427f8
melisanchez2003 <108958674+melisanchez2003@users.noreply.github.com>
2023-11-28 22:21:28 -0500

    Sexto commit

---
+++ Amigos/src/main/java/acti6/amigos/Amigos.java
@@ -0,0 +1,15 @@
+
+package acti6.amigos;
+
+/**
+ *
+ * @author melis
+ */
+
+public class Amigos {
+
+    public static void main(String[] args) {
+        Formulario frm= new Formulario();
+        frm.setVisible(true);
+    }
+}
